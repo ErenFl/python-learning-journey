d5acb21a75005a936a4225481f7c0f4d3e77d1a7
Erendani <erenfg987@gmail.com>
2026-04-19 00:10:22 -0600

    Avance

--- Basico/Modulo_3/05_while.py
+++ Basico/Modulo_3/05_while.py
@@ -13,7 +13,9 @@ principio, el cuerpo podría funcionar continuamente hasta el infinito.
 #Bucle infinito
 #while True:
 #    print("Estoy atrapado dentro de un bucle.")
+''' --------------------------------------------------------------------------- '''
 
+#Ejercicio1:
 # Almacena el actual número más grande aquí.
 largest_number = -999999999
 
@@ -32,4 +34,32 @@ while number != -1:
 # Pero si el número es igual a -1
 # Imprime el número más grande.
 print("El número más grande es:", largest_number)
+''' --------------------------------------------------------------------------- '''
+
+#Ejercicio2;
+# Un programa que lee una secuencia de números
+# y cuenta cuántos números son pares y cuántos son impares.
+# El programa termina cuando se ingresa un cero.
+
+odd_numbers = 0
+even_numbers = 0
+
+# Lee el primer número.
+number = int(input("Introduce un número o escribe 0 para detener: "))
+
+# 0 termina la ejecución.
+while number != 0:
+    # Verificar si el número es impar.
+    if number % 2 == 1:
+        # Incrementar el contador de números impares odd_numbers.
+        odd_numbers += 1
+    else:
+        # Incrementar el contador de números pares even_numbers.
+        even_numbers += 1
+    # Leer el siguiente número.
+    number = int(input("Introduce un número o escribe 0 para detener: "))
+
+# Imprimir resultados.
+print("Conteo de números impares:", odd_numbers)
+print("Conteo de números pares:", even_numbers)
 
---
+++ Basico/Modulo_3/05_while_ejercicio.py
@@ -0,0 +1,9 @@
+counter = 5
+
+#El contador comienza con 5
+while counter != 0:
+    print("Dentro del bucle.", counter) #imprime el contador
+    counter -= 1 #Le resta 1 a cada bucle  que pasa
+print("Fuera del bucle.", counter) #sale del bucle cuando counter sea 0
+
+#LEGIBILIDAD
